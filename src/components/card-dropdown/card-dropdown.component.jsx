7340e57bc5082dd41bc18dbf92ff39e153543d9f
avigailRavia <avigailravia@gmail.com>
2022-06-06 12:57:13 +0300

    added in our addToCart feature

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -1,13 +1,26 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart';
 
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './card-dropdown.styles.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
 
   return (
     <div className='cart-dropdown-container'>
-
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
